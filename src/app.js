c5dbd833f2b1aa7fa3a2efca04c47eb05402a80b
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-03-17 22:09:35 -0500

    Integrated the app with Alan AI and executed test command

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react"; //to initialize the "alanBtn" or Alan AI.
+
+const alanKey ="f555823e5ab06d0c56e4b19c35eb81262e956eca572e1d8b807a3e2338fdd0dc/stage"; //Alan AI API key from the Alan Studio
+
+const App = () =>{
+
+  useEffect( ()=> {
+
+    alanBtn({
+      
+      key: alanKey, 
+      onCommand: ({command}) => {
+        if(command === "testCommand")alert("Test Command Successfull");
+      }
+
+
+    });//This is the API key for Alan AI.
+
+  }, []);
+
+  return (
+    <div>
+      <h1>Alan AI News Application</h1>
+    </div>
+  );
+}
+
+export default App;
